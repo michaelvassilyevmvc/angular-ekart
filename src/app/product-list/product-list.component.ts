107b6f641d968910bf8da271db72807d1147b580
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-02-13 20:51:24 +0500

    End of Lesson 20. Изучен ngFor. Динамический вывод mainMenu

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,34 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent
 {
-  addToCart: number = 0;
-  product = {
-    name: 'Phone XL',
-    price: 799,
-    color: 'Red',
-    discounted: 8.5,
-    inStock: 10,
-    pImage: '/assets/images/iphone.png'
-  }
-
-  getDiscountedPrice()
-  {
-    return this.product.price - (this.product.price * this.product.discounted / 100)
-  }
-
-  inputName(event: any)
-  {
-  }
-
-  increaseAddToCart()
-  {
-    if (this.product.inStock > this.addToCart)
-      this.addToCart++;
-  }
-
-  discreaseAddToCart()
-  {
-    if (this.addToCart > 0)
-      this.addToCart--;
-  }
+  listOfString: string[] = ['Mark', 'Steve', 'Merry', 'John', 'Sarah'];
 }
